3fe6362080850df7f8a4b601f885105d09d263f3
Guilherme Almeida <guilherme.almeida@desinel.com>
2024-03-20 22:19:22 +0000

    Trust me bro commit

--- evo_algorithm.py
+++ evo_algorithm.py
@@ -1,14 +1,33 @@
-def gen_individual_rep1():
+import random
 
-    return 0
+run = {
+    'n_steps' : 0,
+    'reward' : 0,
+    'last_observation' : 0,
+    
+    
+}
+
+def gen_individual_rep1():
+    
+    ind = {
+        'genotype' : [],
+        'fitness' : 0
+    }
+    
+    for i in range(16):
+        ind['genotype'].append(random.randint(0,3))
+        
+    return ind
 
 def tournament_selection(population, tournament_size):
 
     return 0
 
-def function_fitness(population):
+def function_fitness(ind, run):
+    
+    
 
-    return 0
 
 def elitism(population):
 
@@ -17,7 +36,3 @@ def elitism(population):
 def gen_desc(population, n_parents):
 
     return 0
-
-def mapping(genotype):
-
-    return 0 
